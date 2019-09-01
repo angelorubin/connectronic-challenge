7cf190fec6a5e526da9efa4bd952f3718bf91aa0
angelorubin <angelorubin@gmail.com>
2019-09-01 11:47:30 -0300

    Criando lista de categorias

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,17 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import { Dashboard } from "./dashboard/Dashboard";
-import { List } from "../components/list/List";
 import { Login } from "./login/Login";
-// import { UsersList } from "./user/UsersList";
+import { Dashboard } from "./dashboard/Dashboard";
+import { ListCategories } from "../components/category/ListCategories";
 
 export const Root = () => {
   return (
     <Router>
       <Route exact path="/" component={Login} />
       <Route path="/dashboard" component={Dashboard} />
-      <Route path="/list" component={List} />
-      <Route path="/users-list" component={null} />
+      <Route path="/list-categories" component={ListCategories} />
     </Router>
   );
 };
