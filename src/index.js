ef64b4a92d60de2283eab22b2b7796d59a302ef0
angelorubin <angelorubin@gmail.com>
2019-09-04 18:16:42 -0300

    Implementando redux

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { render } from "react-dom";
 import { Root } from "./components/Root";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 render(
-  <>
+  <Provider store={store}>
     <CssBaseline />
     <Root />
-  </>,
+  </Provider>,
   document.querySelector("#root")
 );
