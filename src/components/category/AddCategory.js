973738227f4580224869febcad70ad214bb84ddb
angelorubin <angelorubin@gmail.com>
2019-09-04 10:03:33 -0300

    Refatorando componente AddCategory

--- src/components/category/AddCategory.js
+++ src/components/category/AddCategory.js
@@ -1,61 +1,90 @@
-import React, { useState } from "react";
+import React from "react";
 import {
-  Button,
+  Grid,
   Icon,
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogContentText,
+  Typography,
   TextField,
-  DialogActions
+  Paper,
+  Button,
+  makeStyles
 } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import { Header } from "../header/Header";
 
-export const AddCategory = ({ onClick }) => {
-  const [open, setOpen] = useState(false);
+const useStyles = makeStyles({
+  root: {
+    marginTop: "4rem",
+    padding: "2rem"
+  },
+  containerButton: {
+    display: "flex",
+    width: "100%"
+  },
+  itemButton: {
+    marginRight: "auto"
+  }
+});
 
-  const handleClickOpen = () => setOpen(true);
-
-  const handleClose = () => setOpen(false);
-
+export const AddCategory = () => {
+  const classes = useStyles();
   return (
-    <>
-      <Button
-        onClick={handleClickOpen}
-        variant="outlined"
-        size="small"
-        color="primary"
-      >
-        ADICIONAR <Icon fontSize="small">add</Icon>
-      </Button>
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="form-dialog-title"
-      >
-        <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            To subscribe to this website, please enter your email address here.
-            We will send updates occasionally.
-          </DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Email Address"
-            type="email"
-            fullWidth
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            Cancel
-          </Button>
-          <Button onClick={handleClose} color="primary">
-            Subscribe
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </>
+    <Header>
+      <Paper className={classes.root}>
+        <Grid
+          container
+          spacing="3"
+          direction="column"
+          justify="center"
+          alignItems="center"
+        >
+          <Grid item xs={12}>
+            <Icon fontSize="large" color="primary">
+              add_circle
+            </Icon>
+          </Grid>
+
+          <Grid item xs={12}>
+            <Typography variant={"h6"}>Criar Categoria</Typography>
+          </Grid>
+
+          <Grid container spacing={1}>
+            <Grid item xs={6}>
+              <TextField
+                fullWidth
+                autoFocus
+                margin="dense"
+                id="name"
+                label="Nome"
+                type="text"
+                variant="outlined"
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                fullWidth
+                margin="dense"
+                id="description"
+                label="Descrição"
+                type="text"
+                variant="outlined"
+              />
+            </Grid>
+          </Grid>
+
+          <Grid container>
+            <Grid item xs>
+              <Button variant="outlined" component={Link} to="/list-categories">
+                Voltar
+              </Button>
+            </Grid>
+            <Grid item justify="flex-end">
+              <Button variant="outlined" onClick={""}>
+                Salvar
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Paper>
+    </Header>
   );
 };
