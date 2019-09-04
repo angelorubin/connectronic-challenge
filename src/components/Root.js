835a76dbc936381eaed20ea52670ae1488821e5e
angelorubin <angelorubin@gmail.com>
2019-09-04 09:47:39 -0300

    Criando rotas

--- src/components/Root.js
+++ src/components/Root.js
@@ -2,14 +2,18 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Login } from "./login/Login";
 import { Dashboard } from "./dashboard/Dashboard";
-import { ListCategories } from "../components/category/ListCategories";
+import { AddCategory } from "./category/AddCategory";
+import { ListCategories } from "./category/ListCategories";
+import { EditCategory } from "./category/EditCategory";
 
 export const Root = () => {
   return (
     <Router>
       <Route exact path="/" component={Login} />
       <Route path="/dashboard" component={Dashboard} />
+      <Route path="/add-category" component={AddCategory} />
       <Route path="/list-categories" component={ListCategories} />
+      <Route path="/edit-category" component={EditCategory} />
     </Router>
   );
 };
