127bdabcf86ab115f57d96cf2cf5838944f306ff
angelorubin <angelorubin@gmail.com>
2019-09-04 18:45:09 -0300

    Criando reducer de categorias

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from "redux";
 
 import { userReducer } from "./userReducer";
 import { postReducer } from "./postReducer";
+import { categoriesReducer } from "./categoriesReducer";
 
 export const reducers = combineReducers({
   users: userReducer,
-  posts: postReducer
+  posts: postReducer,
+  categories: categoriesReducer
 });
