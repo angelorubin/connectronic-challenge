f522ec45cb157b917662a7a79ba655b0780e5543
angelorubin <angelorubin@gmail.com>
2019-09-04 10:19:35 -0300

    Refatorando componente Login

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,17 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Grid, Button, makeStyles } from "@material-ui/core";
+import { Grid, Button, TextField, makeStyles } from "@material-ui/core";
 import { blue } from "@material-ui/core/colors";
 
 const useStyles = makeStyles({
-  root: {},
-  divider: {
-    marginTop: 1,
-    color: "red",
-    width: "10vw"
+  root: {
+    margin: 0,
+    width: "100vw",
+    height: "100vh"
   },
-  grid: {
-    minHeight: "100vh"
+  border: {
+    borderColor: blue[50]
   }
 });
 
@@ -19,28 +18,39 @@ export const Login = () => {
   const classes = useStyles();
   return (
     <Grid
-      container
       spacing={2}
+      className={classes.root}
+      container
       direction="column"
       alignItems="center"
       justify="center"
-      className={`${classes.grid} ${classes.root}`}
     >
       <Grid item>
         <h2>Connectronic</h2>
       </Grid>
       <Grid item>
-        <label> email </label>
-        <input />
+        <TextField
+          fullWidth
+          id="email"
+          label="email"
+          className={classes.border}
+          margin="dense"
+          variant="outlined"
+        />
       </Grid>
       <Grid item>
-        <label> password </label>
-        <input />
+        <TextField
+          id="password"
+          label="senha"
+          className={classes.border}
+          margin="dense"
+          variant="outlined"
+        />
       </Grid>
       <Grid item>
         <Button
           color="primary"
-          variant="inset"
+          variant="contained"
           component={Link}
           to={"/dashboard"}
         >
