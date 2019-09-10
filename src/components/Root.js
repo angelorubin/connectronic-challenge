2d8b9c007d1b85f30896de0fbbc079e2fa768353
angelorubin <angelorubin@gmail.com>
2019-09-10 17:16:06 -0300

    Alterando o caminho do componente

--- src/components/Root.js
+++ src/components/Root.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Login } from "./login/Login";
 import { Dashboard } from "./dashboard/Dashboard";
-import { AddCategory } from "./category/AddCategory";
+import { CreateCategory } from "../components/create-category";
 import { ListCategories } from "./category/ListCategories";
 import { EditCategory } from "./category/EditCategory";
 
@@ -11,7 +11,7 @@ export const Root = () => {
     <Router>
       <Route exact path="/" component={Login} />
       <Route path="/dashboard" component={Dashboard} />
-      <Route path="/add-category" component={AddCategory} />
+      <Route path="/create-category" component={CreateCategory} />
       <Route path="/list-categories" component={ListCategories} />
       <Route path="/edit-category" component={EditCategory} />
     </Router>
