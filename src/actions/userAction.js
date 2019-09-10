06de9ffc7d662446a06ee5e0aa8753f804d4eda2
angelorubin <angelorubin@gmail.com>
2019-09-10 17:11:54 -0300

    Arquivos desnecessários deletados

--- src/actions/userAction.js
+++
@@ -1,8 +0,0 @@
-import { ADD_USER } from "../constants/actionTypes";
-
-export const addUser = (dispatch, payload) => {
-  dispatch({
-    type: ADD_USER,
-    payload
-  });
-};
--- src/constants/actionTypes.js
+++
@@ -1,4 +0,0 @@
-export const ADD_USER = "ADD_USER";
-export const FETCH_ALL_USERS = "FETCH_ALL_USERS";
-export const ADD_POST = "ADD_POST";
-export const ADD_CATEGORY = "ADD_CATEGORY";
