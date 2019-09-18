d8592438188f060bc86028ba8de04afae12f6cf4
angelorubin <angelorubin@gmail.com>
2019-09-18 14:36:42 -0300

    Fixando inconsistências no layout do login, criando links para registro e recuperação de senha

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,56 +1,99 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { makeStyles, Grid, Button, TextField } from "@material-ui/core";
+import {
+  makeStyles,
+  Grid,
+  Button,
+  TextField,
+  Paper,
+  Container,
+  Typography,
+  Link as MuiLink
+} from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    padding: theme.spacing(3, 2)
+  container: {
+    display: "flex",
+    height: "100vh",
+    width: "100vw",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  item: {
+    margin: "0.8rem"
   }
 }));
 
 export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const classes = useStyles();
 
+  const handleChange = e => {
+    if (e.target.name === "email") {
+      setEmail(e.target.value);
+      console.log(email);
+    }
+    if (e.target.name === "password") {
+      setPassword(e.target.value);
+      console.log(password);
+    }
+  };
+
+  const handleClick = e => {
+    console.log(email, password);
+  };
+
   return (
-    <Grid
-      spacing={2}
-      className={classes.root}
-      container
-      direction="column"
-      alignItems="center"
-      justify="center"
-    >
-      <Grid item>
-        <h2>React Redux MUI Panel</h2>
+    <Grid className={classes.container}>
+      <Grid className={classes.item}>
+        <Typography variant="h4">Login</Typography>
       </Grid>
-      <Grid item>
+      <Grid className={classes.item}>
         <TextField
           fullWidth
+          name="email"
           id="email"
           label="email"
           className={classes.border}
           margin="dense"
           variant="outlined"
+          value={email}
+          onChange={handleChange}
         />
       </Grid>
-      <Grid item>
+      <Grid className={classes.item}>
         <TextField
           id="password"
-          label="senha"
+          name="password"
+          label="password"
           className={classes.border}
           margin="dense"
           variant="outlined"
+          value={password}
+          onChange={handleChange}
         />
       </Grid>
-      <Grid item>
+      <Grid className={classes.item}>
         <Button
           color="primary"
           variant="contained"
           component={Link}
-          to={"/dashboard"}
+          onClick={handleClick}
         >
-          entrar
+          Sign In
         </Button>
+      </Grid>
+      <Grid className={classes.item}>
+        <Typography>
+          <Link to="/register">register</Link>
+        </Typography>
+      </Grid>
+      <Grid className={classes.item}>
+        <Typography>
+          <Link to="/lost-password">lost password?</Link>
+        </Typography>
       </Grid>
     </Grid>
   );
