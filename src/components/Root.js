4b05e9bed6c92822f8710c4a262d7c2a1345ca5b
angelorubin <angelorubin@gmail.com>
2019-09-18 14:07:57 -0300

    Adicionando rotas para registro e página não encontrada

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,19 +1,25 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Login } from "./login/Login";
 import { Dashboard } from "./dashboard/Dashboard";
-import { CreateCategory } from "../components/create-category";
+import { CreateCategory } from "./category/CreateCategory";
 import { ListCategories } from "./category/ListCategories";
 import { EditCategory } from "./category/EditCategory";
+import { Register } from "./register/Register";
+import { PageNotFound } from "./page-not-found/PageNotFound";
 
 export const Root = () => {
   return (
     <Router>
-      <Route exact path="/" component={Login} />
-      <Route path="/dashboard" component={Dashboard} />
-      <Route path="/create-category" component={CreateCategory} />
-      <Route path="/list-categories" component={ListCategories} />
-      <Route path="/edit-category" component={EditCategory} />
+      <Switch>
+        <Route exact path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/create-category" component={CreateCategory} />
+        <Route path="/list-categories" component={ListCategories} />
+        <Route path="/edit-category" component={EditCategory} />
+        <Route component={PageNotFound} />
+      </Switch>
     </Router>
   );
 };
