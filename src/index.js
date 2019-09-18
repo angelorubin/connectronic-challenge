0e7b4afa8f2c9c403e36480821d421c2150ac858
angelorubin <angelorubin@gmail.com>
2019-09-18 13:57:21 -0300

    Renomeando a store

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import { render } from "react-dom";
 import { Root } from "./components/Root";
 import { Provider } from "react-redux";
-import { store } from "./store";
+import { storeConfig as store } from "./store/storeConfig";
 
 render(
   <Provider store={store}>
